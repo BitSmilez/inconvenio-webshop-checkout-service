ef2e4895b8a373aae453cf78a936caf92ce4e7c0
Biri <test@email>
2022-11-30 23:24:58 +0100

    init

---
+++ src/main/java/com/bitsmilez/checkoutmicroservice/CheckoutMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bitsmilez.checkoutmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitsmilez/checkoutmicroservice/CheckoutMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitsmilez.checkoutmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
