b9bd1d59be3865f8be78d6f844d8bf29647fd299
Biri <test@email>
2023-02-19 18:37:34 +0100

    INCONV-47 [+] service returns statuscode

--- src/main/java/com/bitsmilez/checkoutmicroservice/port/MQAdapter/CheckoutConsumer.java
+++ src/main/java/com/bitsmilez/checkoutmicroservice/port/MQAdapter/CheckoutConsumer.java
@@ -3,9 +3,11 @@ package com.bitsmilez.checkoutmicroservice.port.MQAdapter;
 
 import com.bitsmilez.checkoutmicroservice.config.MQConfig.CheckoutMessage;
 import com.bitsmilez.checkoutmicroservice.config.MQConfig.MQConfig;
+import com.bitsmilez.checkoutmicroservice.core.domain.service.interfaces.ICheckoutService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,10 +15,22 @@ public class CheckoutConsumer {
 
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CheckoutConsumer.class);
+    private final ICheckoutService checkoutService;
+
+    public CheckoutConsumer(ICheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
 
 
     @RabbitListener(queues = MQConfig.CHECKOUT_QUEUE)
-    public void receiveCheckoutMessage(CheckoutMessage message) {
+    public ResponseEntity<?> receiveCheckoutMessage(CheckoutMessage message) {
+
         LOGGER.info("Received message: {}", message);
+        if (checkoutService.createOrder(message)){
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 }
