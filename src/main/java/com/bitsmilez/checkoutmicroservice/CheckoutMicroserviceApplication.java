e91af32c7e8d7012a9596a1dfdb95e530c2b278e
Biri <test@email>
2023-02-19 17:17:53 +0100

    INCONV-47 [+] added annotation

--- src/main/java/com/bitsmilez/checkoutmicroservice/CheckoutMicroserviceApplication.java
+++ src/main/java/com/bitsmilez/checkoutmicroservice/CheckoutMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.bitsmilez.checkoutmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.bitsmilez.checkoutmicroservice.core.domain.service.interfaces")
 public class CheckoutMicroserviceApplication {
 
 	public static void main(String[] args) {
