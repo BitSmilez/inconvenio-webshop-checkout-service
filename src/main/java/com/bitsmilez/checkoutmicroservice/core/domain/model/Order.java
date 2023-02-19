668a5a805559d58e497b297ba8d0e76880af749f
Biri <test@email>
2023-02-19 11:34:09 +0100

    INCONV-47 [+] added entity/dto

---
+++ src/main/java/com/bitsmilez/checkoutmicroservice/core/domain/model/Order.java
@@ -0,0 +1,30 @@
+package com.bitsmilez.checkoutmicroservice.core.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID orderID;
+    private String address;
+    private String city;
+    private String zip;
+    private String country;
+    private String paymentMethod;
+    private String shippingMethod;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private BigDecimal orderTotal;
+
+}
