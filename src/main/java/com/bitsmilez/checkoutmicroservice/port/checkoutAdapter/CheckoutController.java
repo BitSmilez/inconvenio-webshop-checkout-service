76f1a1553a5248f3b7eeef38b7ed8414274bf221
Biri <test@email>
2023-02-19 17:16:39 +0100

    INCONV-47 [+] added annotation

--- src/main/java/com/bitsmilez/checkoutmicroservice/port/checkoutAdapter/CheckoutController.java
+++ src/main/java/com/bitsmilez/checkoutmicroservice/port/checkoutAdapter/CheckoutController.java
@@ -1,5 +1,8 @@
 package com.bitsmilez.checkoutmicroservice.port.checkoutAdapter;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CheckoutController {
 
     // TODO: GET -> last order (UserID)
