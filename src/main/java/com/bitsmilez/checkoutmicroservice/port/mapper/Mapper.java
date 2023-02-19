1de101ec9991374f5d7f594a2445dce04b7c0ed4
jutnh <jutnhbr@gmail.com>
2023-02-19 01:28:31 +0100

    INCONV-47 [+] Empty Mapper Class

---
+++ src/main/java/com/bitsmilez/checkoutmicroservice/port/mapper/Mapper.java
@@ -0,0 +1,10 @@
+package com.bitsmilez.checkoutmicroservice.port.mapper;
+
+public class Mapper {
+
+    // checkoutMessageToOrderEntity / DTO
+    // Same fields as checkoutMessage + generated OrderID and generated current Date ->  Instant.now().toString(),
+
+
+
+}
