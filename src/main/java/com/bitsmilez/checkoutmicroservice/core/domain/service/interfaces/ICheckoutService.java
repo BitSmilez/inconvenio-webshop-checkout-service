c60011184225bec66ef3ec64573262bc327e3899
Biri <test@email>
2023-02-19 11:53:36 +0100

    INCONV-47 [+] added interface methods

--- src/main/java/com/bitsmilez/checkoutmicroservice/core/domain/service/interfaces/ICheckoutService.java
+++ src/main/java/com/bitsmilez/checkoutmicroservice/core/domain/service/interfaces/ICheckoutService.java
@@ -2,13 +2,17 @@ package com.bitsmilez.checkoutmicroservice.core.domain.service.interfaces;
 
 import com.bitsmilez.checkoutmicroservice.config.MQConfig.CheckoutMessage;
 
+import java.util.UUID;
+
 public interface ICheckoutService {
 
     // Create Checkout (CheckoutMessage)
     void createOrder(CheckoutMessage checkoutMessage);
 
     // GetLatestCheckout (UserID)
+    void getLatestCheckout(UUID userID);
 
     // GetAllCheckouts (UserID)
+    void getAllCheckouts(UUID userID);
 
 }
