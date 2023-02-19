8ab9f420b7de85c1e73a8ff86fdf4d58b234c3aa
jutnh <jutnhbr@gmail.com>
2023-02-19 01:30:25 +0100

    INCONV-47 [+] Empty Controller

---
+++ src/main/java/com/bitsmilez/checkoutmicroservice/port/checkoutAdapter/CheckoutController.java
@@ -0,0 +1,10 @@
+package com.bitsmilez.checkoutmicroservice.port.checkoutAdapter;
+
+public class CheckoutController {
+
+    // TODO: GET -> last order (UserID)
+
+    // TODO GET -> All Orders (UserID)
+
+
+}
