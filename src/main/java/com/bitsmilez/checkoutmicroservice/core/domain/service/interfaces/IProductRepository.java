dec1a5520286180f4f8390e45ba0008cdd3643a5
Biri <test@email>
2023-02-20 16:31:51 +0100

    INCONV-47 [+] added JPA Repository

---
+++ src/main/java/com/bitsmilez/checkoutmicroservice/core/domain/service/interfaces/IProductRepository.java
@@ -0,0 +1,11 @@
+package com.bitsmilez.checkoutmicroservice.core.domain.service.interfaces;
+
+import com.bitsmilez.checkoutmicroservice.core.domain.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IProductRepository extends JpaRepository<Product, UUID> {
+}
